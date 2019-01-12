5a44a1c8a238f7c7400da0d071fac25517b34d6d
Garima Chhikara <garimachhikara128@gmail.com>
2019-01-12 21:35:52 +0530

    Lec18 : Generic Tree

--- src/L17_Jan6/GTClient.java
+++ src/L17_Jan6/GTClient.java
@@ -12,8 +12,22 @@ public class GTClient {
 	public static void main(String[] args) {
 
 		// 10 3 20 2 50 0 60 0 30 1 70 0 40 2 80 0 90 0
-		GenericTree gt = new GenericTree();
+		String str = "10 3 20 2 50 0 60 0 30 1 70 0 40 2 80 0 90 0";
+		GenericTree gt = new GenericTree(str);
 		gt.display();
+		System.out.println(gt.size());
+		System.out.println(gt.max());
+		System.out.println(gt.find(500));
+		System.out.println(gt.ht());
+		// gt.mirror();
+		gt.display();
+		gt.preorder();
+		gt.postorder();
+		gt.levelOrder();
+		gt.levelOrderLW();
+		gt.levelOrderZZ();
+
+		gt.printAtLevel(2);
 	}
 
 }
--- src/L17_Jan6/GenericTree.java
+++ src/L17_Jan6/GenericTree.java
@@ -1,6 +1,7 @@
 package L17_Jan6;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 /**
@@ -12,7 +13,7 @@ import java.util.Scanner;
 
 public class GenericTree {
 
-	Scanner scn = new Scanner(System.in);
+	Scanner scn;
 
 	private class Node {
 		int data;
@@ -21,7 +22,8 @@ public class GenericTree {
 
 	private Node root;
 
-	public GenericTree() {
+	public GenericTree(String str) {
+		scn = new Scanner(str);
 		this.root = construct(null, -1);
 	}
 
@@ -61,15 +63,6 @@ public class GenericTree {
 
 		// self work
 
-		// smaller problem
-		// for (Node child : node.children) {
-		// display(child);
-		// }
-
-		for (int i = node.children.size() - 1; i >= 0; i--) {
-			display(node.children.get(i));
-		}
-
 		String str = node.data + " -> ";
 
 		for (Node child : node.children) {
@@ -78,6 +71,263 @@ public class GenericTree {
 		str += ".";
 		System.out.println(str);
 
+		// smaller problem
+		for (Node child : node.children) {
+			display(child);
+		}
+
+		// for (int i = node.children.size() - 1; i >= 0; i--) {
+		// display(node.children.get(i));
+		// }
+
+	}
+
+	public int size() {
+		return size(this.root);
+	}
+
+	private int size(Node node) {
+
+		int sum = 0;
+
+		for (Node child : node.children) {
+			sum += size(child);
+		}
+
+		return sum + 1;
+
+	}
+
+	public int max() {
+		return max(this.root);
+	}
+
+	private int max(Node node) {
+
+		int tm = node.data;
+
+		for (Node child : node.children) {
+			int cm = max(child);
+
+			tm = Math.max(tm, cm);
+		}
+
+		return tm;
+
+	}
+
+	public boolean find(int item) {
+		return find(this.root, item);
+	}
+
+	private boolean find(Node node, int item) {
+
+		if (node.data == item) {
+			return true;
+		}
+
+		for (Node child : node.children) {
+			boolean cf = find(child, item);
+
+			if (cf)
+				return true;
+		}
+
+		return false;
+
+	}
+
+	public int ht() {
+		return ht(this.root);
+	}
+
+	private int ht(Node node) {
+
+		int th = -1;
+
+		for (Node child : node.children) {
+
+			int ch = ht(child);
+			th = Math.max(ch, th);
+		}
+
+		return th + 1;
+	}
+
+	public void mirror() {
+		mirror(this.root);
+	}
+
+	private void mirror(Node node) {
+
+		// self work
+		int l = 0;
+		int r = node.children.size() - 1;
+
+		while (l < r) {
+
+			Node ln = node.children.get(l);
+			Node rn = node.children.get(r);
+
+			node.children.set(r, ln);
+			node.children.set(l, rn);
+
+			l++;
+			r--;
+		}
+
+		for (Node child : node.children) {
+			mirror(child);
+		}
+	}
+
+	public void display2() {
+		display2(this.root);
+	}
+
+	private void display2(Node node) {
+
+		System.out.println("Hii " + node.data);
+
+		for (Node child : node.children) {
+			System.out.println("Going towards " + child.data);
+			display2(child);
+			System.out.println("Coming back from " + child.data);
+
+		}
+
+		System.out.println("Bye " + node.data);
+
+	}
+
+	public void preorder() {
+		preorder(this.root);
+		System.out.println();
+	}
+
+	private void preorder(Node node) {
+
+		System.out.print(node.data + " ");
+
+		for (Node child : node.children) {
+			preorder(child);
+		}
+	}
+
+	public void postorder() {
+		postorder(this.root);
+		System.out.println();
+	}
+
+	private void postorder(Node node) {
+
+		for (Node child : node.children) {
+			postorder(child);
+		}
+
+		System.out.print(node.data + " ");
+
+	}
+
+	public void levelOrder() {
+
+		LinkedList<Node> queue = new LinkedList<>();
+		queue.addLast(this.root);
+
+		while (!queue.isEmpty()) {
+
+			// remove
+			Node rn = queue.removeFirst();
+
+			// print
+			System.out.print(rn.data + " ");
+
+			// child
+			for (Node child : rn.children) {
+				queue.addLast(child);
+			}
+
+		}
+		System.out.println();
+
+	}
+
+	public void levelOrderLW() {
+
+		LinkedList<Node> primary = new LinkedList<>();
+		LinkedList<Node> helper = new LinkedList<>();
+
+		primary.addLast(this.root);
+
+		while (!primary.isEmpty()) {
+
+			Node rn = primary.removeFirst();
+
+			System.out.print(rn.data + " ");
+
+			for (Node child : rn.children) {
+				helper.addLast(child);
+			}
+
+			if (primary.isEmpty()) {
+				System.out.println();
+				primary = helper;
+				helper = new LinkedList<>();
+			}
+
+		}
+
+	}
+
+	public void levelOrderZZ() {
+
+		LinkedList<Node> queue = new LinkedList<>();
+		LinkedList<Node> stack = new LinkedList<>();
+
+		queue.addLast(this.root);
+
+		int count = 0;
+
+		while (!queue.isEmpty()) {
+
+			Node rn = queue.removeFirst();
+
+			System.out.print(rn.data + " ");
+
+			if (count % 2 == 0) {
+				for (Node child : rn.children) {
+					stack.addFirst(child);
+				}
+			} else {
+
+				for (int i = rn.children.size() - 1; i >= 0; i--) {
+					stack.addFirst(rn.children.get(i));
+				}
+			}
+
+			if (queue.isEmpty()) {
+				count++;
+				System.out.println();
+				queue = stack;
+				stack = new LinkedList<>();
+			}
+
+		}
+
+	}
+
+	public void printAtLevel(int level) {
+		printAtLevel(this.root, 0, level);
+	}
+
+	private void printAtLevel(Node node, int count, int level) {
+
+		if (count == level)
+			System.out.println(node.data);
+
+		for (Node child : node.children) {
+			printAtLevel(child, count + 1, level);
+		}
 	}
 
 }
